a6880fafa6928447c169493761ae4670bf35208c
Pavel Larin <www.larinpavel@mail.ru>
2023-05-23 00:01:01 +0500

    PermutationInString.  Time is O(n), where n is the length of s2. Space is O(1), because we only use a constant size array to store teh frequency of characters in s1.

--- src/NeetCode150/3SlidingWindow/PermutationInString.java
+++ src/NeetCode150/3SlidingWindow/PermutationInString.java
@@ -36,7 +36,27 @@ class PermutationInString {
     // Initially, both are at index 0.
     // * We use a while loop to move the right pointer until it reaches the end of s1.
     // In each iteration, we do the following:
-
+    // - We get the character at the right pointer and update the frequency array
+    // by decrementing the corresponding index. For example, if rc = 'e', then freq[4]-.
+    // - We also check if the frequency of this character was positive before decrementing.
+    // This means that this character is present in s1 adn we need to match it in the window.
+    // So we decrement the count variable as well.
+    // For example, if rc = 'a' and freq[0] = 1, then count-- and freq[0] = 0.
+    // - We then increment the right pointer by one.
+    // - We check if the count variable is zero. This means that we have matched all the characters in s1
+    // within the current window. So we return true as we have found a permutation.
+    // - We also check if the window size is equal to the length of s1. This means that we
+    // need to move the left pointer as well to maintain a fixed window size.
+    // So we have to do the following:
+    // -- We get the character at the left pointer and update the frequency array by
+    // incrementing the corresponding index. For example, if lc = 'n', then freq1++.
+    // -- We also check if the frequency of this character was non-negative before incrementing.
+    // This means that this character was present in s1 adn we have removed it from the window.
+    // So we increment the count variable as well.
+    // For example, if lc = 'a' and freq[0] = 0, then count++ and freq[0] = 1.
+    // -- We then increment the left pointer by one.
+    // - If we exit the while loop without returning true, it means
+    // that we have not found any permutation of s1 in s2. So we return false.
 
     public boolean checkInclusion(String s1, String s2) {
         // edge cases
@@ -72,18 +92,10 @@ class PermutationInString {
         return false;
     }
 
+    // The time complexity of this solution is O(n), where n is the length of s2.
+    // This is because we only iterate over each character in s2 once using the sliding window technique.
 
+    // The space complexity of this solution is O(1), because we only use a constant size array to
+    // store the frequency of characters in s1.
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
