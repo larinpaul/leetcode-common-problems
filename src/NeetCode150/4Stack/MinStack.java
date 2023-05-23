cee47c7ba02c228e42a621c7ed188c888a6700e7
Pavel Larin <www.larinpavel@mail.ru>
2023-05-24 01:30:20 +0500

    MinStack. Time complexity of each operation is O(1), since we only perform constant time operations on both stacks. The space complexity is O(n), where n is the number of elements in the main stack, since we use an extra stack of size n/2 on average.

--- src/NeetCode150/4Stack/MinStack.java
+++ src/NeetCode150/4Stack/MinStack.java
@@ -35,27 +35,88 @@
 // * Methods pop, top and getMin operations will always be called on non-empty stacks.
 // * At most 3 * 10^4 calls will be made to push, pop, top, and getMin.
 
+import java.util.Stack;
+
 class MinStack {
 
+    // Here we will need to design a stack that supports push, pop, top, and getMin operations
+    // in constant time. A stack is a data structure that follows the last-in first-out (LIFO) principle,
+    // meaning that the last element pushed onto the stack is the first one popped off.
+    // The getMin operation returns the minimum element in the stack without removing it.
+
+    // One possible solution is to use an auxiliary stack that keeps track of the minimum element
+    // at each level of the main stack. Whenever we push a new element onto the main stack,
+    // we also push the current minimum element onto the auxiliary stack. Whenever we pop an
+    // element form the main stack, we also pop an element from the auxiliary stack.
+    // This way,the top of the auxiliary stack always contains the minimum element in the main stack.
+
+    // Declare two stacks: one for storing element and one for storing minimums
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    // Initialize both stacks
     public MinStack() {
-
+        stack = new Stack<>();
+        minStack = new Stack<>();
     }
 
+    // Push an element onto both stacks
     public void push(int val) {
-
+        // Push val onto the main stack
+        stack.push(val);
+        // If the minStack is empty or val is smaller than or equal to the current minimum,
+        // push val onto the minStack as well
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
     }
 
+    // Pop an element form both stacks
     public void pop() {
-
+        // Pop an element form the main stack and store it in a variable
+        int val = stack.pop();
+        // If the popped element is equal to the current minimum,
+        // pop an element form the minStack as well
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
     }
 
+    // Return the top element of the main stack
     public int top() {
-
+        return stack.peek();
     }
 
+    // Return the top element of the minStack, which is the minimum element of the main stack
     public int getMin() {
-
+        return minStack.peek();
     }
+
+
+
+//    public MinStack() {
+//
+//    }
+//
+//    public void push(int val) {
+//
+//    }
+//
+//    public void pop() {
+//
+//    }
+//
+//    public int top() {
+//
+//    }
+//
+//    public int getMin() {
+//
+//    }
+
+    // The time complexity of each operation is O(1), since we only perform constant time
+    // operations on both stacks. The space complexity is O(n), where n is the number of
+    // elements in the main stack, since we use an extra stack of size n/2 on average.
 
 }
 
