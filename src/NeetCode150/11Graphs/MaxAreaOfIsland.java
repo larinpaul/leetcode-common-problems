06228a3b377e3a7352d95e9f244f9a877b504301
Pavel Larin <www.larinpavel@mail.ru>
2023-06-06 23:06:06 +0500

    MaxAreaOfIsland. Time is O(m * n), Space is O(m * n),because of the recursive call stack. In the worst case, we might have a grid full of 1's, and we need to perform a DFS on every cell.

--- src/NeetCode150/11Graphs/MaxAreaOfIsland.java
+++ src/NeetCode150/11Graphs/MaxAreaOfIsland.java
@@ -63,7 +63,12 @@ class MaxAreaOfIsland {
             // mark the cell as visited by setting it to 0
             grid[i][j] = 0;
             // return 1 plus the sum of DFS calls on adjacent cells
-            return 1 + dfs(grid, i - 1, j) + dfs(grid, i + 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
+            return
+                    1
+                    + dfs(grid, i - 1, j)
+                    + dfs(grid, i + 1, j)
+                    + dfs(grid, i, j - 1)
+                    + dfs(grid, i, j + 1);
         }
         // otherwise, return zero
         else {
